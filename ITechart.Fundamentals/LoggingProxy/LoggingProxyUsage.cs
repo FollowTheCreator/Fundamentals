ae4eb9c4667534429a41b09325ab6208e25e0ee5
Artyom <Artyomkovalevich.top@gmail.com>
2019-07-16 16:39:00 +0300

    add logging proxy usage

---
+++ ITechart.Fundamentals/LoggingProxy/LoggingProxyUsage.cs
@@ -0,0 +1,19 @@
+﻿using ITechart.Fundamentals.LoggingProxy.Implementations;
+using ITechart.Fundamentals.LoggingProxy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITechart.Fundamentals.LoggingProxy
+{
+    class LoggingProxyUsage
+    {
+        public static void UseLoggingProxy()
+        {
+            var writer = new LoggingProxy<IWriter>().CreateInstance(new Writer());
+            writer.Write("Method Execution...");
+        }
+    }
+}
