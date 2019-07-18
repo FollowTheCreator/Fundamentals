ebfc18c90c862cf7ae46dfa7611eeea61d74c355
Artyom <Artyomkovalevich.top@gmail.com>
2019-07-18 14:49:57 +0300

    add CsvEnumerable usage

---
+++ ITechart.Fundamentals/CsvEnumerable/CsvEnumerableUsage.cs
@@ -0,0 +1,19 @@
+﻿using ITechart.Fundamentals.CsvEnumerable.Implementations;
+using ITechart.Fundamentals.CsvEnumerable.Models;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ITechart.Fundamentals.CsvEnumerable
+{
+    class CsvEnumerableUsage
+    {
+        public static void UseCsvEnumerable()
+        {
+            var data = new CsvEnumerableWithHelper<Record>(new StreamReader(@"file.csv"))
+                .Select(row => row.ToString());
+
+            Console.WriteLine(string.Join("\n", data));
+        }
+    }
+}
